b908a9ca166942be80706decdb7e36d65f125386
Kaua Breno <vaibetan@gmail.com>
2025-05-11 16:31:38 -0300

    template laravel criado

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <section id="login">
+        <div class="container">
+            <h1>MaxFood</h1>
+            <img src="images/logo-mx.png" alt="">
+            <p>Email</p>
+            <input type="text">
+            <p>Senha</p>
+            <input type="text">
+            <button type="submit">Entrar</button>
+        </div>
+    </section>
+    
+</body>
+</html>
