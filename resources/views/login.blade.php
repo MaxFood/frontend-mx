923a63053bd369fc3795736d49fb11d954a8084a
Kaua Breno <vaibetan@gmail.com>
2025-05-19 01:41:19 -0300

    Pagina de Login feita

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,19 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/__reset.css">
+    <link rel="stylesheet" href="css/login/login.css">
 </head>
 <body>
-    <section id="login">
-        <div class="container">
-            <h1>MaxFood</h1>
-            <img src="images/logo-mx.png" alt="">
-            <p>Email</p>
-            <input type="text">
-            <p>Senha</p>
-            <input type="text">
+    <div class="container">
+        <h1>MAX<br><span>FOOD</span></h1>
+        <img src="images/logo-mx.png" alt="">
+        <form action="">
+            <input type="text" placeholder="Email">
+            <input type="text" placeholder="Senha">
             <button type="submit">Entrar</button>
-        </div>
-    </section>
-    
+        </form>
+    </div>
 </body>
 </html>
